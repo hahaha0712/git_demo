2d9d59b41dd555d1286e587f36f3ea99a9be016f
Ha <toilaha0712@gmail.com>
2021-09-09 12:08:05 +0700

    them cau chui bay

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("ay zo wtf");
         System.out.println("say hello to Ha");
+        System.out.println("hello con cac");
     }
 }
